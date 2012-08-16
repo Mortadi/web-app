76379ce494b28b454e58f18d720d69ee41bff88c
Mortadi <mortadi1330@yahoo.com>
2012-08-16 18:29:36 -0400

    Working on notes.

--- index.php
+++ index.php
@@ -145,7 +145,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         	
             <button type="submit" class="submit">Submit</button> 
         </form>
-      <a href="notes.php"><button>Notes</button></a> </div>
+      <a href="notes.php"><button id="noteBtn">Notes</button></a> </div>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
 <script src="js/general.js"></script>
