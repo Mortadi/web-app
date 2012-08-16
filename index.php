5c34ca1f4054c9e32b8c7ee8c61582c9e8e98ca7
Mortadi <mortadi1330@yahoo.com>
2012-08-16 18:05:48 -0400

    still moving my button around

--- index.php
+++ index.php
@@ -119,7 +119,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <h2>Your Fluent Journal</h2>
 </header>
 
-<a class="notesbutton" href="notes.php"><button>Notes</button></a> 
+<a href="notes.php"><button>Notes</button></a> 
 <div class="main">
  <form id="notes" action="index.php" method="post">
         
