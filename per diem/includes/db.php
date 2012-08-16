1cfad5e68857ad85ea110b2fe507e9d680a1aca3
Mortadi <mortadi1330@yahoo.com>
2012-08-16 17:40:46 -0400

    modifying my db.php to upload it to phpfog

--- per diem/includes/db.php
+++ per diem/includes/db.php
@@ -7,4 +7,4 @@ $name = getenv('MYSQL_DB_NAME');
 
 $data_source = sprintf('mysql:host=%s;dbname=%s', $host, $name);
 $db = new PDO($data_source, $user, $pass);
-$db->exec('SET NAMES utf8');
+$db->exec('SET NAMES utf8'); 
